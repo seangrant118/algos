5b71b6da62663fb56abfd05b0a5988a16b42c9bb
Sean Grant <seangrant118@gmail.com>
2020-05-18 11:12:02 -0400

    reverse string test

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,11 @@
+// Given a string, return a new string with the reversed order of characters
+
+// Examples ---
+// reverse('apple') === leppa
+// reverse('hello') === 'olleh
+
+function reverse(str) {
+  //
+}
+
+module.exports = reverse;
