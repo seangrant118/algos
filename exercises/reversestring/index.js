dd8b835c6102e7a04f87f4b5e60096cf7e00a905
Sean Grant <seangrant118@gmail.com>
2020-05-18 11:31:19 -0400

    reverse1

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -4,8 +4,15 @@
 // reverse('apple') === leppa
 // reverse('hello') === 'olleh
 
-function reverse(str) {
-  //
+function reverse1(str) {
+  // turns the string into an array
+  const arr = str.split("");
+
+  //reverses array using built in array.reverse method
+  arr.reverse();
+
+  return arr.join("");
 }
 
-module.exports = reverse;
+//testing
+console.log(reverse1("hello"));
