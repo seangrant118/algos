b588ea30b7fbc45ca1e86d227b1b09816b163d75
Sean Grant <seangrant118@gmail.com>
2020-05-20 10:07:26 -0400

    maxchar2

--- exercises/maxchars/index.js
+++ exercises/maxchars/index.js
@@ -24,4 +24,25 @@ function maxChar(str) {
   return maxChar;
 }
 
+function maxChar2(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 console.log(maxChar("asdasdhaksdhkajsdhjkhhhhhhhhhhhhhhhhhh"));
+console.log(
+  maxChar2("asdasdhaksdhkajjjjjjjjjjjjjjjjjjjjjjjsdhjkhhhhhhhhhhhhhhhhhh")
+);
