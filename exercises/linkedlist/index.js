b8358001d9c6638c6168073b6ca2756cc98d7897
Sean Grant <seangrant118@gmail.com>
2020-05-28 18:21:13 -0400

    insert at

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -119,4 +119,20 @@ class LinkedList {
 
     prev.next = prev.next.next;
   }
+
+  insertAt(data, i) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if (i === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const prev = this.getAt(i - 1) || this.getLast();
+    const node = new Node(data, prev.next);
+    prev.next = node;
+  }
 }
