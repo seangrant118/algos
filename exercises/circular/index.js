9a2f7d424257241980bc9d4a6306e7ba8d3c81b2
Sean Grant <seangrant118@gmail.com>
2020-06-02 18:07:22 -0400

    circular

---
+++ exercises/circular/index.js
@@ -0,0 +1,15 @@
+function circular(list) {
+  let fast = list.getFirst();
+  let slow = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
