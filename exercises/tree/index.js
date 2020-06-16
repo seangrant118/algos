22c731cb8ee53fdd0e7689bf8e9720b7d9cd2e1f
Sean Grant <seangrant118@gmail.com>
2020-06-16 12:21:05 -0400

    df traversal

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -30,4 +30,15 @@ class Tree {
       fn(node);
     }
   }
+
+  traverseDF(fn) {
+    const arr = [this.root];
+
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
 }
