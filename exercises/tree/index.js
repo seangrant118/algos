8d8d669e4c1f81bc195aaa77373d98c21fc0ed1a
Sean Grant <seangrant118@gmail.com>
2020-06-16 11:59:06 -0400

    node remove

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -7,6 +7,12 @@ class Node {
   add(data) {
     this.children.push(new Node(data));
   }
+
+  remove(data) {
+    this.children = this.children.filter((node) => {
+      return node.data !== data;
+    });
+  }
 }
 
 class Tree {}
