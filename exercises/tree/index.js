d659cb9442016c0d4526e82657bd2ae3ba381c06
Sean Grant <seangrant118@gmail.com>
2020-06-16 12:17:39 -0400

    bf traversal

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -19,4 +19,15 @@ class Tree {
   constructor() {
     this.root = null;
   }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
 }
