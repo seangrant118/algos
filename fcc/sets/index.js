e4beffaca19351ac990f10a53fdebbc8c263257c
Sean Grant <seangrant118@gmail.com>
2020-07-13 11:40:53 -0400

    value add remove size

--- fcc/sets/index.js
+++ fcc/sets/index.js
@@ -10,4 +10,32 @@ function mySet() {
   this.has = function (el) {
     return collection.indexOf(el) !== -1;
   };
+
+  // this method will return all values in the set
+  this.values = function () {
+    return collection;
+  };
+
+  // this method will add an el to set
+  this.add = function (el) {
+    if (!this.has(el)) {
+      collection.push(el);
+      return true;
+    }
+    return false;
+  };
+
+  // remove an el
+  this.remove = function (el) {
+    if (this.has(el)) {
+      index = collection.indexOf(el);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
 }
