f73f2e21be050a055cbbced9a573de14d1e9cdcf
Sean Grant <seangrant118@gmail.com>
2020-07-14 09:58:24 -0400

    n queue

--- fcc/queue/index.js
+++ fcc/queue/index.js
@@ -8,4 +8,16 @@ function Queue() {
   this.enqueue = function (el) {
     collection.push(el);
   };
+  this.dequeue = function () {
+    return collection.shift();
+  };
+  this.front = function () {
+    return collection[0];
+  };
+  this.size = function () {
+    return collection.length;
+  };
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
 }
