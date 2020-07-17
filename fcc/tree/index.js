7f4be5148d468a30b7847e91b469e204b547090e
Sean Grant <seangrant118@gmail.com>
2020-07-17 17:47:38 -0400

    remove

--- fcc/tree/index.js
+++ fcc/tree/index.js
@@ -78,5 +78,38 @@ class BST {
         current = current.right;
       }
     }
+    return false;
+  }
+  remove(data) {
+    const removeNode = function (node, data) {
+      if (node === null) {
+        return null;
+      }
+      if (data === node.data) {
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        if (node.left == null) {
+          return node.right;
+        }
+        if (node.right == null) {
+          return node.left;
+        }
+        var tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.root = removeNode(this.root, data);
   }
 }
