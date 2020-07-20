55b0a894fb22f5d08a2a65be47c5f6690a793f5b
Sean Grant <seangrant118@gmail.com>
2020-07-20 10:18:02 -0400

    find max height

--- fcc/tree/index.js
+++ fcc/tree/index.js
@@ -127,4 +127,16 @@ class BST {
       return right + 1;
     }
   }
+  findMaxHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+    if (left > right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
 }
