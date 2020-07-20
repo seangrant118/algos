917aa7c1cc8290947bea800f2187313c7ad8cf5c
Sean Grant <seangrant118@gmail.com>
2020-07-20 10:30:53 -0400

    level order

--- fcc/tree/index.js
+++ fcc/tree/index.js
@@ -167,4 +167,24 @@ class BST {
       return result;
     }
   }
+  levelOrder() {
+    let result = [];
+    let Q = [];
+    if (this.root !== null) {
+      Q.push(this.root);
+      while (Q.length > 0) {
+        let node = Q.shift();
+        result.push(node.data);
+        if (node.left != null) {
+          Q.push(node.left);
+        }
+        if (node.right != null) {
+          Q.push(node.right);
+        }
+      }
+      return result;
+    } else {
+      return null;
+    }
+  }
 }
