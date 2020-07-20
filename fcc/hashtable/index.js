b44a01ece11a3d013e59a25405260f5aa12ca1ac
Sean Grant <seangrant118@gmail.com>
2020-07-20 11:12:13 -0400

    hash table add

--- fcc/hashtable/index.js
+++ fcc/hashtable/index.js
@@ -7,3 +7,30 @@ var hash = (str, max) => {
   }
   return hash % max;
 };
+
+let hashTable = function () {
+  let storage = [];
+  const storageLimit = 4;
+
+  this.print = function () {
+    console.log(storage);
+  };
+
+  this.add = function (key, value) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      storage[index] = [[key, value]];
+    } else {
+      var inserted = false;
+      for (let i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
+};
