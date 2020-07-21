b8abb35db707a93030d45326c093bb6099efdaf3
Sean Grant <seangrant118@gmail.com>
2020-07-21 11:12:11 -0400

    lookup

--- fcc/hashtable/index.js
+++ fcc/hashtable/index.js
@@ -39,9 +39,22 @@ let hashTable = function () {
     if (storage[index].length === 1 && Storage[index][0][0] === key) {
       delete storage[index];
     } else {
-      for (let i = 0; i < storage[index]; i++) {
+      for (let i = 0; i < storage[index].length; i++) {
         if (storage[index][i][0] === key) {
           delete storage[index][i];
+        }
+      }
+    }
+  };
+
+  this.lookup = function (key) {
+    let index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (let i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
         }
       }
     }
