23d29f2ce9d6bb2e4f5b4589faa255b691ca67c2
Sean Grant <seangrant118@gmail.com>
2020-07-22 09:40:24 -0400

    ll

---
+++ fcc/linkedlist/index.js
@@ -0,0 +1,19 @@
+// Linked List
+
+function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function (element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function () {
+    return length;
+  };
+
+  this.head = function () {
+    return head;
+  };
+}
