86ad5c403c2efa4e362f7948b2d41b189bc74de2
Sean Grant <seangrant118@gmail.com>
2020-07-23 18:30:05 -0400

    trie node

---
+++ fcc/trie/index.js
@@ -0,0 +1,10 @@
+let Node = function () {
+  this.keys = new Map();
+  this.end = false;
+  this.setEnd = function () {
+    this.end = true;
+  };
+  this.isEnd = function () {
+    return this.end;
+  };
+};
