e7ddc09c0f1831f896e1335f23d6a50862d84da0
Sean Grant <seangrant118@gmail.com>
2020-08-26 11:41:41 -0400

    sort and remove arr

--- misc/arrays.js
+++ misc/arrays.js
@@ -3,6 +3,7 @@
 //
 
 //Brute force
+//n^2
 let a = [1, 2, 5, 2, 1, 8];
 
 let b = [];
@@ -11,5 +12,17 @@ let len = a.length;
 for (let i = 0; i < len; i++) {
   if (b.indexOf(a[i]) === -1) {
     b.push(a[i]);
+  }
+}
+
+// SOrt and REmove
+// n+n
+a.sort();
+let _temp;
+
+for (let i = 0; i < len; i++) {
+  if (a[i] !== _temp) {
+    b.push(a[i]);
+    _temp = a[i];
   }
 }
