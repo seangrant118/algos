5d657a0b571fb7a49ae35743864741e07a34b6b6
Sean Grant <seangrant118@gmail.com>
2020-09-04 14:59:19 -0400

    prime factors

---
+++ tjsd/primefactors/index.js
@@ -0,0 +1,14 @@
+function primeFactors(n) {
+  let factors = [];
+  let divisor = 2;
+
+  while (n > 2) {
+    if (n % divisor == 0) {
+      factors.push(divisor);
+      n = n / divisor;
+    } else {
+      divisor++;
+    }
+  }
+  return factors;
+}
