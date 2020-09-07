109e2c11524affca4d1e71dc95f9c6c7d937e5ea
Sean Grant <seangrant118@gmail.com>
2020-09-07 12:59:20 -0400

    fib

---
+++ tjsd/fib/index.js
@@ -0,0 +1,11 @@
+function fib(n) {
+  let sequence = [0, 1];
+
+  if (n <= 2) return 1;
+
+  for (let i = 2; i <= n; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+
+  return sequence[n];
+}
