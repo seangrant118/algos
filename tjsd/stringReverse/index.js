f630307d9936a6df2c692309173eb19611d9559d
Sean Grant <seangrant118@gmail.com>
2020-09-14 15:47:33 -0400

    non concat

--- tjsd/stringReverse/index.js
+++ tjsd/stringReverse/index.js
@@ -2,8 +2,21 @@
 
 function reverse(str) {
   var rtnStr = "";
-  for (var i = str.length - 1; i >= 0; i--) {
+  for (let i = str.length - 1; i >= 0; i--) {
     rtnStr += str[i];
   }
   return rtnStr;
+}
+
+// non concat
+
+function reverse(str) {
+  var reversed = [];
+
+  if (!str || typeof str != "string" || str.length < 2) return str;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed.push(str[i]);
+  }
+  return reversed.join("");
 }
