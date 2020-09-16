1235b44a229b6823613c5a5e114610c5631c62fb
Sean Grant <seangrant118@gmail.com>
2020-09-16 14:52:29 -0400

    reverse words logn

---
+++ tjsd/reverseWord/index.js
@@ -0,0 +1,16 @@
+// long
+
+function reverseWords(str) {
+  let rev = [];
+  let wordLen = 0;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] == " " || i == 0) {
+      rev.push(str.substr(i, wordLen + 1));
+      wordLen = 0;
+    } else {
+      wordLen++;
+    }
+  }
+  return rev.join(" ");
+}
