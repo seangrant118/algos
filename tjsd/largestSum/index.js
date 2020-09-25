6cd49b62ed248083c3acee6b11a7573f7bb82713
Sean Grant <seangrant118@gmail.com>
2020-09-25 15:18:24 -0400

    largest sum

---
+++ tjsd/largestSum/index.js
@@ -0,0 +1,23 @@
+function largestSum(arr) {
+  let biggest = arr[0];
+  let second = arr[1];
+  let len = arr.length;
+
+  if (len < 2) return null;
+
+  if (biggest < second) {
+    biggest = arr[1];
+    second = arr[0];
+  }
+
+  for (let i = 2; i < len; i++) {
+    if (arr[i] > biggest) {
+      second = biggest;
+      biggest = arr[i];
+    } else if (arr[i] > second) {
+      second = arr[i];
+    }
+  }
+
+  return biggest + second;
+}
