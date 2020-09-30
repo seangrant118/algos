c162f8fd44cd5dc1848113e56cbdfb4b4d39ffaf
Sean Grant <seangrant118@gmail.com>
2020-09-30 13:53:30 -0400

    pathsCounter

--- adv/roboPaths/index.js
+++ adv/roboPaths/index.js
@@ -16,4 +16,33 @@ var MakeGrid = function (n) {
   grid.visited = function (i, j) {
     return grid[i][j];
   };
+
+  return grid;
+};
+
+var pathsCounter = function (n) {
+  var paths = 0;
+  const grid = MakeGrid(n);
+
+  var findPaths = function (i, j) {
+    if (i === n - 1 && j === n - 1) {
+      paths++;
+      return;
+    }
+    if (i < 0 || i >= n || j < 0 || j >= n) {
+      return;
+    }
+    if (grid.visited(i, j)) {
+      return;
+    } else {
+      grid.toggle(i, j);
+      findPaths(i, j + 1);
+      findPaths(i + 1, j);
+      findPaths(i, j - 1);
+      findPaths(i - 1, j);
+      grid.toggle(i, j);
+    }
+  };
+  findPaths(0, 0);
+  return paths;
 };
