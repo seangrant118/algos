917e07cdadc64ae8437650188df9c7a0c9000310
Sean Grant <seangrant118@gmail.com>
2020-09-30 13:46:02 -0400

    make grid

---
+++ adv/roboPaths/index.js
@@ -0,0 +1,11 @@
+// how many possible paths can a robot travel to a designated tile, without returning to a tile twice
+
+var MakeGrid = function (n) {
+  var grid = [];
+  for (let i = 0; i < n; i++) {
+    grid.push([]);
+    for (let j = 0; j < n; j++) {
+      grid[i].push(false);
+    }
+  }
+};
