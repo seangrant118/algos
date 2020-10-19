3fbac54b389726900a0432bd01e66c795f7fc507
Sean Grant <seangrant118@gmail.com>
2020-10-19 14:16:41 -0400

    even occurences

---
+++ adv/evenOccurence/index.js
@@ -0,0 +1,32 @@
+//Even occurance
+//build a counting array to count all occurences in the array
+
+// do a for in loop to see if which are even, and push the even to an array
+
+//use math.min to search for the lowest index number that each number in the result array is found in the original arary and return that number
+
+const evenOccurance = function (str) {
+  const arr = str.split("");
+  let occurances = {};
+  let first;
+  let result = [];
+
+  arr.forEach((item) => {
+    if (!occurances[item]) {
+      occurences[item] = 1;
+      return;
+    }
+    occurences[item]++;
+  });
+  for (let key in occurances) {
+    if (occurances[key] % 2 === 0) {
+      result.push(key);
+    }
+  }
+  result.forEach((item) => {
+    if (first === undefined) {
+      first = item;
+    }
+    first = result.indexOf(item) < result.indexOf(first) ? item : first;
+  });
+};
