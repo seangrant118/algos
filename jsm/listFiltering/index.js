1d0d80c520a9c33657ff3ed3f0ee8a66c00ba31c
Sean Grant <seangrant118@gmail.com>
2020-11-11 16:12:13 -0500

    listfiletring

---
+++ jsm/listFiltering/index.js
@@ -0,0 +1,36 @@
+//Given a string, replace every letter with its position in the alphabet
+alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const alphabetPosition = (text) => {
+  return text
+    .toLowerCase()
+    .replace(/[^a-zA-Z]/g, "")
+    .split()
+    .map((letter) => alphabet.indexOf(letter));
+};
