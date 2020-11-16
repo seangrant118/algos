a66cb6724f0573fcaf7d287c8bb12f507e93a49b
Sean Grant <seangrant118@gmail.com>
2020-11-16 14:13:31 -0500

    weakset weakmap

---
+++ dataStructures/weakSetMap/index.js
@@ -0,0 +1,5 @@
+//WeakSet and WeakMap
+
+//Variations of set and map
+//values and keys are only weakly referenced
+//garbage collection can delete values and keys if not used anywhere else in the app
