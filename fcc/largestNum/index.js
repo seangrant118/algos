9fb4ba925d74a3095f313796ddf5accc8a13fe4d
Sean Grant <seangrant118@gmail.com>
2020-12-14 11:55:21 -0500

    largestnum

---
+++ fcc/largestNum/index.js
@@ -0,0 +1,23 @@
+function largestOfFOur(arr) {
+  var maxes = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      let tempMax = arr[i][0];
+      let currentEl = arr[i][j];
+
+      if (currentEl >= tempMax) {
+        tempMax = currentEl;
+      }
+    }
+    maxes.push(tempMax);
+  }
+  return maxes;
+}
+
+largestOfFOur([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 859, 1],
+]);
