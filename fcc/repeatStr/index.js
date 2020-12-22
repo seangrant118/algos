28f0635feb88ce612804ea6c4ec83a1db6edb990
Sean Grant <seangrant118@gmail.com>
2020-12-22 11:23:39 -0500

    recursive

--- fcc/repeatStr/index.js
+++ fcc/repeatStr/index.js
@@ -1,15 +1,24 @@
 function repeatStr(str, n) {
-  if (num < 0) return "";
+  if (n < 0) return "";
 
   return str.repeat(n);
 }
 
 function repeatStr2(str, n) {
   let final = "";
-  if (num < 0) return "";
+  if (n < 0) return "";
 
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < n; i++) {
     final += str;
   }
   return final;
+}
+
+//Recursion
+function repeatStr3(str, n) {
+  if (n < 0) return "";
+
+  if (n === 1) return str;
+
+  return str + repeatStr3(str, num - 1);
 }
