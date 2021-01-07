d9d86e40dba75439056985ab93bcb9db79515634
Sean Grant <seangrant118@gmail.com>
2021-01-07 18:46:56 -0500

    mutation

---
+++ fcc/mutations/index.js
@@ -0,0 +1,13 @@
+// return true if the string in the first el of the array contains all of the letters of the second str
+
+function mutation(arr) {
+  var firstWord = arr[0].toLowerCase();
+  var secondWord = arr[2].toLowerCase();
+
+  for (let i = 0; i < secondWord.length; i++) {
+    if (firstWord.indexOf(secondWord[i]) < 0) {
+      return false;
+    }
+  }
+  return true;
+}
