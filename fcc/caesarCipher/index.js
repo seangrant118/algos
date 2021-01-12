6a75fe7fd546f677c871e79381587b9ffd76b4c4
Sean Grant <seangrant118@gmail.com>
2021-01-12 13:29:42 -0500

    caesar cipher

---
+++ fcc/caesarCipher/index.js
@@ -0,0 +1,17 @@
+// write a function that deciphers a rot13 cipher
+
+function rot13(str) {
+  let solved = "";
+  for (let i = 0; i < str.length; i++) {
+    let asciiNum = str[i].charCodeAt();
+    if (asciiNum >= 65 && asciiNum <= 77) {
+      solved += String.fromCharCode(asciiNum + 13);
+    } else if (asciiNum >= 78 && asciiNum <= 90) {
+      solved += String.fromCharCode(asciiNum - 13);
+    } else {
+      solved += str[i];
+    }
+  }
+
+  return solved;
+}
