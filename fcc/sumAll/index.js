bd61f371d49d10ca19c20d166547d037de8a9de6
Sean Grant <seangrant118@gmail.com>
2021-01-12 14:21:34 -0500

    sum all

---
+++ fcc/sumAll/index.js
@@ -0,0 +1,12 @@
+function sumAll(arr) {
+  let start = Math.min(arr[0], arr[1]);
+  let end = Math.max(arr[0], arr[1]);
+
+  let total = 0;
+
+  for (var i = start; i <= end; i++) {
+    total += i;
+  }
+
+  return total;
+}
