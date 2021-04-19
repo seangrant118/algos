28584024e75993af79a82d1753ffb9e9da8b1d87
Sean Grant <seangrant118@gmail.com>
2021-04-19 14:51:46 -0400

    truncate str

--- fcc/truncateStr/index.js
+++ fcc/truncateStr/index.js
@@ -1,5 +1,8 @@
-function truncateStr(str, num) {
-  if (num <= 3) return str.slice(0, num) + "...";
-
-  return str.slice(0, num - 3) + "...";
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  }
+  return str;
 }
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
