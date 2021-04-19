0f010bb03f5eceecdc90872c7d5b25bed34af450
Sean Grant <seangrant118@gmail.com>
2021-04-19 15:11:25 -0400

    finders keepers

---
+++ fcc/findersKeepers/index.js
@@ -0,0 +1,13 @@
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    console.log(num);
+    if (func(arr[i])) {
+      return num;
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
