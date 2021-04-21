2cca9d7666b16fd9d6c6ec381339f105b4e080bf
Sean Grant <seangrant118@gmail.com>
2021-04-21 13:17:05 -0400

    seek and destroy3

--- fcc/seekAndDestroy/index.js
+++ fcc/seekAndDestroy/index.js
@@ -23,4 +23,9 @@ function destroyer2(arr) {
   });
 }
 
-destroyer([1, 2, 3, 4, 5], 2, 3); // returns [1,4,5]
+// single line
+
+function destroyer3(arr, ...valsToRemove) {
+  return arr.filter((el) => !valsToRemove.includes(el));
+}
+console.log(destroyer3([1, 2, 3, 4, 5], 2, 3)); // returns [1,4,5]
