c752771aebe298d50b8c00bcf4d371c6796ad7bf
Sean Grant <seangrant118@gmail.com>
2021-05-03 16:04:54 -0400

    missing letters

---
+++ fcc/missingLetters/index.js
@@ -0,0 +1,11 @@
+function missingLetters(str) {
+  for (let i = 0; i < str.length; i++) {
+    let code = str.charCodeAt(i);
+
+    if (code !== str.charCodeAt(0) + i) {
+      return String.fromCharCode(code - 1);
+    }
+  }
+
+  return undefined;
+}
