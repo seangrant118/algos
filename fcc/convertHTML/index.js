cf0f3c0854cc46679e65ed174986f7c5fa1bc961
Sean Grant <seangrant118@gmail.com>
2021-05-05 16:12:09 -0400

    convert to html

---
+++ fcc/convertHTML/index.js
@@ -0,0 +1,14 @@
+function convertHTML(str) {
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;",
+  };
+
+  return str
+    .split("")
+    .map((entity) => htmlEntities[entity] || entity)
+    .join("");
+}
