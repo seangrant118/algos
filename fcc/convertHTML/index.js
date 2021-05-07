4bba30fb147f209019ed2c17535de7fe2b475c28
Sean Grant <seangrant118@gmail.com>
2021-05-07 10:53:47 -0400

    sum fib

--- fcc/convertHTML/index.js
+++ fcc/convertHTML/index.js
@@ -21,6 +21,6 @@ function convertHTMLregex(str) {
     '"': "&quot;",
     "'": "&apos;",
   };
-  // Using a regex, replace characters with it's corresponding html entity
+
   return str.replace(/([&<>\"'])/g, (match) => htmlEntities[match]);
 }
---
+++ fcc/sumAllFib/index.js
@@ -0,0 +1,14 @@
+function sumFib(num) {
+  let prev = 0;
+  let curr = 1;
+  let result = 0;
+
+  while (curr <= num) {
+    if (curr % 2 !== 0) {
+      result += curr;
+    }
+    curr += prev;
+    prev = curr - prev;
+  }
+  return result;
+}
