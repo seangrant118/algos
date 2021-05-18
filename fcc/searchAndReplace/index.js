7a323e79c7dd5fc6e23fdee33219344f5cf8b7b2
Sean Grant <seangrant118@gmail.com>
2021-05-18 09:41:29 -0400

    search and replace2

--- fcc/searchAndReplace/index.js
+++ fcc/searchAndReplace/index.js
@@ -8,3 +8,19 @@ function myReplace(str, before, after) {
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+function myReplace(str, before, after) {
+  let index = str.indexOf(before);
+  // Check to see if the first letter is uppercase or not
+  if (str[index] === str[index].toUpperCase()) {
+    // Change the after word to be capitalized before we use it.
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    // Change the after word to be uncapitalized before we use it.
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  // Now replace the original str with the edited one.
+  str = str.replace(before, after);
+
+  return str;
+}
