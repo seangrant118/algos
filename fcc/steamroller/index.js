a616041ddf2989757ff7ae7b96c8861cadd0b341
Sean Grant <seangrant118@gmail.com>
2021-05-26 14:15:17 -0400

    steamroll 3

--- fcc/steamroller/index.js
+++ fcc/steamroller/index.js
@@ -18,3 +18,21 @@ function steamroll2(arr) {
 
   return flat.some(Array.isArray) ? steamroll2(flat) : flat;
 }
+
+function steamroll3(arr) {
+  return arr
+    .toString()
+    .replace(",,", ",") // "1,2,,3" => "1,2,3"
+    .split(",") // ['1','2','3']
+    .map(function (v) {
+      if (v == "[object Object]") {
+        // bring back empty objects
+        return {};
+      } else if (isNaN(v)) {
+        // if not a number (string)
+        return v;
+      } else {
+        return parseInt(v); // if a number in a string, convert it
+      }
+    });
+}
