844f2f8e0d15e4f5cf3976dcdbf7200a10f50097
Sean Grant <seangrant118@gmail.com>
2021-05-27 11:58:51 -0400

    binary2

--- fcc/convertBinary/index.js
+++ fcc/convertBinary/index.js
@@ -8,3 +8,28 @@ function binaryAgent(str) {
 
   return uniString.join("");
 }
+
+function binaryAgent2(str) {
+  str = str.split(" ");
+  var power;
+  var decValue = 0;
+  var sentence = "";
+
+  for (var s = 0; s < str.length; s++) {
+    // Check each bit from binary number
+    for (var t = 0; t < str[s].length; t++) {
+      // This only takes into consideration the active ones.
+      if (str[s][t] == 1) {
+        // This is quivalent to 2 ** position
+        power = Math.pow(2, +str[s].length - t - 1);
+        decValue += power;
+      }
+    }
+
+    sentence += String.fromCharCode(decValue);
+
+    decValue = 0;
+  }
+
+  return sentence;
+}
