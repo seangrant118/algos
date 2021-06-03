fc74bb2dfc76e959318913195ce31c0b68709fde
Sean Grant <seangrant118@gmail.com>
2021-06-03 14:31:08 -0400

    option arg2

--- fcc/optionalArguments/index.js
+++ fcc/optionalArguments/index.js
@@ -29,3 +29,22 @@ function addTogether() {
     return undefined;
   }
 }
+
+function addTogether2() {
+  const [first, second] = Object.values(arguments);
+  // Check first argument
+  if (typeof first !== "number") {
+    return undefined;
+  }
+
+  // Function to add second argument
+  const addSecond = (second) =>
+    typeof second === "number" ? first + second : undefined;
+
+  // Check second argument
+  if (second !== undefined) {
+    return addSecond(second);
+  } else {
+    return addSecond;
+  }
+}
