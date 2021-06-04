7e94bb41bb3bc86bb32e4ea4371629e5dbbd2ee0
Sean Grant <seangrant118@gmail.com>
2021-06-04 14:01:44 -0400

    optional args 3

--- fcc/optionalArguments/index.js
+++ fcc/optionalArguments/index.js
@@ -48,3 +48,12 @@ function addTogether2() {
     return addSecond;
   }
 }
+
+function addTogether3() {
+  var args = Array.from(arguments);
+  return args.some((n) => typeof n !== "number")
+    ? undefined
+    : args.length > 1
+    ? args.reduce((acc, n) => (acc += n), 0)
+    : (n) => (typeof n === "number" ? n + args[0] : undefined);
+}
